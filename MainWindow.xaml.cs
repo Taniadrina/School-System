7a8aa4b9ad9dd0e28c798308f5c10bc49dcf75dc
Taniadrina <32650197+Taniadrina@users.noreply.github.com>
2018-03-29 11:32:01 -0600

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using LIVEX.Class;
+using LIVEX.UserControl;
+using LIVEX.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LIVEX
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string user_name;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            lblFechaHeader.Content = lblFechaHeader.Content + DateTime.Today.ToLongDateString();
+
+        }
+
+        public MainWindow(string user_name)
+        {
+            InitializeComponent();
+            this.user_name = user_name;
+            lblBienvenida.Content = lblBienvenida.Content +" "+ user_name;
+            lblFechaHeader.Content = lblFechaHeader.Content + DateTime.Today.ToLongDateString();
+
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void brd_S_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximizae_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                grd_AnaGridWindow.Margin = new Thickness(0,0,0,0);
+            }
+            else {
+                this.WindowState = WindowState.Normal;
+                grd_AnaGridWindow.Margin = new Thickness(15);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new MainView());
+        }
+
+        private void btnLogo_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new MainView());
+        }
+
+        private void menuBtn_Alumnos_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new Alumnos());
+        }
+
+        private void menuBtn_Maestros_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new MaestrosView());
+        }
+
+        private void menuBtn_Grupos_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new GruposView());
+        }
+
+        private void menuBtn_Ganancias_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new GananciasView());
+        }
+
+        private void menuBtn_Ajustes_Click(object sender, RoutedEventArgs e)
+        {
+            Vistas.add_View(Content, new AjustesView());
+        }
+    }
+}
---
+++ login.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LIVEX
+{
+    /// <summary>
+    /// Lógica de interacción para login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void on_click_button(object sender, RoutedEventArgs e)
+        {
+            //Obtenemos los datos introducidos
+            string nb_introducido = TextBox_Usuario.Text;
+            string password_introducido = TextBox_Contraseña.Text;
+
+            //Utilizamos el modelo de entidades
+            livexEntities modeloEntities = new livexEntities();
+            //livexDataSet modeloEntities = new livexDataSet();
+
+            //VERIFICAMOS SI EL USUARIO EXISTE EN LA db
+
+            user usuario = modeloEntities.user.SingleOrDefault(us => us.user_name.Equals(nb_introducido));
+
+            if (usuario != null)
+            {
+                //Verificamos las contraseñas
+                if (usuario.user_password.Equals(password_introducido))
+                {
+                    //MainWindow mainWindow = new MainWindow();
+                    //mainWindow.Show();//Abre la nueva ventana
+                    MainWindow v = new MainWindow(usuario.user_name);
+                    v.Show();
+                    this.Close();//Cierra la ventana actual
+
+                }
+                else
+                {
+                    //Las contraseñas no coinciden
+                    if (usuario.user_name.Equals(nb_introducido))
+                    {
+                        TextBox_Usuario.Background = Brushes.White; //Cambiamos el color del textbox
+                    }
+
+                    TextBox_Contraseña.Background = Brushes.Red; //Cambiamos el color del textbox
+                    TextBlock_MensajeError.Text = "La contraseña no es correcta"; //Establecemos el mensaje de error
+                    TextBox_Contraseña.Clear(); //Borramos la contraseña introducida
+
+                }
+            }
+            //El usuario no existe
+            else
+            {
+                TextBox_Usuario.Background = Brushes.Red; //Cambiamos el color del Textbox
+                TextBox_Usuario.Clear(); //Limiamos el valor introducido
+                TextBox_Contraseña.Clear();
+                TextBlock_MensajeError.Text = "El usuario introducido no es correcto";//establecemos el mensaje de error
+
+            }
+            //MainWindow mainWindow = new MainWindow();
+            //mainWindow.Show(); //muestra la nueva ventana
+            //this.Close(); //cierra la ventana actual
+        }
+
+        private void cancel_click_button(object sender, RoutedEventArgs e)
+        {
+            this.Close(); //Cierra la ventana actual
+        }
+    }
+}
